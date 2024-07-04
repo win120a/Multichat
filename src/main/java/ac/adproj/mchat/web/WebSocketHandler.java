f07654650c045634f62bdf832215aa13a3f60f0c
Andy Cheung <micro_zx@msn.cn>
2024-07-04 16:24:33 +0800

    Cleanup commit of WebSocketHandler.

--- src/main/java/ac/adproj/mchat/web/WebSocketHandler.java
+++ src/main/java/ac/adproj/mchat/web/WebSocketHandler.java
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2011-2020 Andy Cheung
+    Copyright (C) 2011-2024 Andy Cheung
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -30,27 +30,24 @@ import org.eclipse.jetty.websocket.api.WebSocketListener;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
 
 import static ac.adproj.mchat.handler.MessageType.REGISTER;
 import static ac.adproj.mchat.handler.MessageType.USERNAME_QUERY_REQUEST;
+import static ac.adproj.mchat.model.Protocol.*;
 
 /**
  * Message Handler of WebSocket.
- * 
+ *
  * @author Andy Cheung
  * @since 2020/5/18
  */
 @Slf4j
 public class WebSocketHandler implements WebSocketListener {
-    private Session session;
-    private String uuid;
-    private String nickname;
-
-    private static Set<WebSocketHandler> connections;
-    
-    public static boolean isConnected() {
-        return !connections.isEmpty();
-    }
+    private static final Pattern PATTERN_OF_PRIVATE_CHATTING_MESSAGE = Pattern.compile("[@].*[#]");
+    private static final Set<WebSocketHandler> connections;
+    // UUID, Name
+    private static final Map<String, String> nameBindings;
 
     static {
         connections = Collections.synchronizedSet(new HashSet<>(16));
@@ -58,31 +55,13 @@ public class WebSocketHandler implements WebSocketListener {
         MessageDistributor.getInstance().registerSubscriber(new WebSocketBridge());
     }
 
-    {
-        uuid = UUID.randomUUID().toString();
-    }
+    private final String uuid = UUID.randomUUID().toString();
+    private Session session;
+    private String nickname;
 
-    public static class WebSocketBridge implements SubscriberCallback {
-        @Override
-        public void onMessageReceived(String uiMessage) {
-            String name = uiMessage.split("\\s*:\\s*")[0];
-            
-            for (WebSocketHandler conn : connections) {
-                if (conn.nickname.equals(name) || conn.uuid.equals(name)) {
-                    continue;
-                }
-                
-                try {
-                    conn.session.getRemote().sendString(uiMessage);
-                } catch (IOException e) {
-                    log.error("Failed in sending websocket message in broadcasting. [UUID = " + conn.uuid + "]", e);
-                }
-            }
-        }
+    public static boolean isConnected() {
+        return !connections.isEmpty();
     }
-
-    // UUID, Name
-    private static Map<String, String> nameBindings;
 
     @Override
     public void onWebSocketBinary(byte[] payload, int offset, int len) {
@@ -103,54 +82,91 @@ public class WebSocketHandler implements WebSocketListener {
 
     @Override
     public void onWebSocketText(String message) {
+        var um = UserManager.getInstance();
+
         switch (MessageType.getMessageType(message)) {
-            case DEBUG:
-            case KEEP_ALIVE:
-                break;
 
-            case INCOMING_MESSAGE:
-                String protoMsg = message.replace(uuid, nickname);
-                
-                try {
-                    MessageDistributor.getInstance().sendRawProtocolMessage(protoMsg);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-                
-                broadcastMessage(protoMsg);
-                break;
+            case INCOMING_MESSAGE -> handleIncomingMessage(message, um);
 
-            case REGISTER:
-                Map<String, String> result = REGISTER.tokenize(message);
+            case REGISTER -> handleRegister(message);
 
-                if (UserManager.getInstance().reserveName(result.get("name"))) {
-                    nameBindings.put(result.get("uuid"), result.get("name"));
-                }
+            case USERNAME_QUERY_REQUEST -> handleUsernameQueryRequest(message);
 
-                nickname = result.get("name");
-
-                break;
-
-            case USERNAME_QUERY_REQUEST:
-                String name = USERNAME_QUERY_REQUEST.tokenize(message).get("username");
-
-                try {
-                    session.getRemote()
-                            .sendString(UserManager.getInstance().containsName(name) ? Protocol.USER_NAME_DUPLICATED
-                                    : Protocol.USER_NAME_NOT_EXIST);
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
-                break;
-
-            case UNKNOWN:
-            default:
-                // Broadcast message directly.
-                broadcastMessage(message);
-                break;
+            default -> {
+                // Don't do anything.
+            }
         }
+    }
+
+    private void handleUsernameQueryRequest(String message) {
+        String name = USERNAME_QUERY_REQUEST.tokenize(message).get("username");
+
+        try {
+            session.getRemote()
+                    .sendString(UserManager.getInstance().containsName(name) ? Protocol.USER_NAME_DUPLICATED
+                            : Protocol.USER_NAME_NOT_EXIST);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void handleRegister(String message) {
+        Map<String, String> result = REGISTER.tokenize(message);
+
+        if (UserManager.getInstance().reserveName(result.get("name"))) {
+            nameBindings.put(result.get("uuid"), result.get("name"));
+        }
+
+        nickname = result.get("name");
+    }
+
+    private void handleIncomingMessage(String message, UserManager um) {
+        String protoMsg = message.replace(uuid, nickname);
+
+        var matcherOfMessageText = PATTERN_OF_PRIVATE_CHATTING_MESSAGE.matcher(message);
+
+        // Private chatting message.
+        if (matcherOfMessageText.find()) {
+            var targetName = matcherOfMessageText.group()
+                    .replace("@", "")
+                    .replace("#", "");
+
+            connections.stream()
+                    .filter(x -> x.nickname.equals(targetName))
+                    .findFirst()
+                    .ifPresentOrElse(x -> {
+                        try {
+                            x.session.getRemote().sendString(protoMsg);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }, () -> {
+                        try {
+                            var tokenizedMessage = MessageType.INCOMING_MESSAGE.tokenize(message);
+
+                            var messageText = tokenizedMessage.get("messageText").split("#", 2)[1];
+
+                            ServerListener.getInstance().sendCommunicationData(MESSAGE_HEADER_LEFT_HALF +
+                                    this.nickname + " -> " + targetName + " (私聊)" +
+                                    MESSAGE_HEADER_MIDDLE_HALF + MESSAGE_HEADER_RIGHT_HALF +
+                                    messageText, um.findUuidByName(targetName).get());
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+            return;
+        }
+
+        try {
+            MessageDistributor.getInstance().sendRawProtocolMessage(protoMsg);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+
+        broadcastMessage(protoMsg);
     }
 
     @Override
@@ -164,8 +180,6 @@ public class WebSocketHandler implements WebSocketListener {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        // broadcastMessage(uuid);
     }
 
     private void broadcastMessage(String message) {
@@ -181,7 +195,7 @@ public class WebSocketHandler implements WebSocketListener {
                 }
             }
         }
-        
+
         try {
             ServerListener.getInstance().sendCommunicationData(message, Protocol.BROADCAST_MESSAGE_UUID);
         } catch (IOException e) {
@@ -194,7 +208,7 @@ public class WebSocketHandler implements WebSocketListener {
     public int hashCode() {
         return uuid.hashCode();
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -206,8 +220,32 @@ public class WebSocketHandler implements WebSocketListener {
         if (getClass() != obj.getClass()) {
             return false;
         }
-        
+
         WebSocketHandler other = (WebSocketHandler) obj;
         return Objects.equals(uuid, other.uuid);
+    }
+
+    public static class WebSocketBridge implements SubscriberCallback {
+        @Override
+        public void onMessageReceived(String uiMessage) {
+
+            if (uiMessage.isEmpty()) {
+                return;
+            }
+
+            String name = uiMessage.split("\\s*:\\s*")[0];
+
+            for (WebSocketHandler conn : connections) {
+                if (conn.nickname.equals(name) || conn.uuid.equals(name)) {
+                    continue;
+                }
+
+                try {
+                    conn.session.getRemote().sendString(uiMessage);
+                } catch (IOException e) {
+                    log.error("Failed in sending websocket message in broadcasting. [UUID = " + conn.uuid + "]", e);
+                }
+            }
+        }
     }
 }
